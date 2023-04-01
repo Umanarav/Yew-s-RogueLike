6c50b1d4f083cf5adf9f847c80fe731dd8e33397
Umanarav <norahnewcomb@gmail.com>
2023-04-01 15:00:26 -0500

    working on intro cutscene

--- js/animation.js
+++ js/animation.js
@@ -96,7 +96,27 @@ const titleBackdrop6 = new Image();
 
 var someNumberTitle = 11;
 
-animatingTitle = false;
+let animatingTitle = false;
+
+let starHue = 0;
+
+// Load the images
+const layer1 = new Image();
+layer1.src = 'images/layer1.png';
+const layer2 = new Image();
+layer2.src = 'images/layer2.png';
+const layer3 = new Image();
+layer3.src = 'images/layer3.png';
+
+// Set the initial x positions of the layers
+let layer1X = 0;
+let layer2X = 0;
+let layer3X = 0;
+
+// Set the speeds of the layers
+const layer1Speed = 1;
+const layer2Speed = 2;
+const layer3Speed = 3;
 
 function drawTitleBackdrop() {
     if (animatingTitle === true){
@@ -107,6 +127,35 @@ function drawTitleBackdrop() {
 
         ctx.fillStyle = 'rgba(0,0,0,.75)';
         ctx.fillRect(0,0,canvas.width, canvas.height);
+
+       // Draw the layers
+  
+       /*for (let i = 0; i < Math.ceil(canvas.width / layer2.width) + 1; i++) {
+                ctx.drawImage(layer2, layer2X + i * layer2.width, 0);
+            }
+        for (let i = 0; i < Math.ceil(canvas.width / layer3.width) + 1; i++) {
+            ctx.drawImage(layer3, layer3X + i * layer3.width, 0);
+        }
+
+        for (let i = 0; i < Math.ceil(canvas.width / layer1.width) + 1; i++) {
+            ctx.drawImage(layer1, layer1X + i * layer1.width, 0);
+        }*/
+
+        // Update the layer positions based on their speeds
+        layer1X -= layer1Speed;
+        layer2X -= layer2Speed;
+        layer3X -= layer3Speed;
+
+        // If a layer goes off the screen to the left, reset it to the end of the canvas
+        if (layer1X + layer1.width < 0) {
+            layer1X = layer1X + layer1.width;
+        }
+        if (layer2X + layer2.width < 0) {
+            layer2X = layer2X + layer2.width;
+        }
+        if (layer3X + layer3.width < 0) {
+            layer3X = layer3X + layer3.width;
+        }
 
         //animate rainbow frame
         if (someNumberTitle === 11 || someNumberTitle === 12){
@@ -132,6 +181,8 @@ function drawTitleBackdrop() {
             someNumberTitle = 11
             ctx.drawImage(titleBackdrop6, 0, 0,)
         }
+
+        //ctx.drawImage(boss2b, bouncingX, bouncingY, 144 + someNumberTitle, 144 + someNumberTitle);
 
         drawText("Yew's", 42, true, canvas.height/2 - 55, "black");
         drawText("Rogue-Like", 72, true, canvas.height/2 - 5, "black");
